004cfaecb356cddd7466bf7fea97a4632997e7a1
Steven20201 <khantzawhein261@example.com>
2021-11-21 12:01:42 +0800

    my first project

---
+++ script.js
@@ -0,0 +1,8 @@
+$( ".change" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+      $( "body" ).removeClass( "dark" );
+
+    } else {
+      $( "body" ).addClass( "dark" );
+    }
+});
